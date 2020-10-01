31947de63ec9bc9f0dc2a9a516698c7341bbd4b0
Diana1888 <56414696+Diana1888@users.noreply.github.com>
2020-09-30 22:01:01 -0400

    Задание 2. Расстояние между двумя точками

---
+++ Sandbox/src/main/java/ru/sandbox/Pointdistance.java
@@ -0,0 +1,11 @@
+package ru.sandbox;
+
+public class Pointdistance {
+
+    public static void main(String[] args) {
+        Point p=new Point(1, 1, 5, 5);
+        System.out.println("Distance between two points = "+ p.distance());
+        }
+}
+
+
