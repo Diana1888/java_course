2ce66de9d7df973f565fe134e16eb1680c4af9cf
Diana1888 <56414696+Diana1888@users.noreply.github.com>
2020-10-04 20:44:32 -0400

    Исправлено задание № 2

--- Sandbox/src/main/java/ru/sandbox/Pointdistance.java
+++ Sandbox/src/main/java/ru/sandbox/Pointdistance.java
@@ -3,8 +3,9 @@ package ru.sandbox;
 public class Pointdistance {
 
     public static void main(String[] args) {
-        Point p=new Point(1, 1, 5, 5);
-        System.out.println("Distance between two points = "+ p.distance());
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(3, 4);
+        System.out.println("Distance between two points = "+ p1.distance(p2));
         }
 }
 
